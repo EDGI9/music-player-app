ca2a2040a7c3e967675366dd64aa0abfab49ffc4
Edgar Dias <dias.s.edgar@gmail.com>
2022-10-02 16:51:16 +0100

    Adding logic to control the progress of the progress circle component WIP

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -9,11 +9,41 @@ function Player(props) {
     const global = useSelector(state => state.global)
     const dispatch = useDispatch()
     const myAudio = useRef();
+    let timer
+  
+    //Set duration of track
+    const onLoadedMetadata = () => {
+        if (myAudio.current) {
+            dispatch({ type: 'player/SET_DURATION', payload: Math.floor(myAudio.current.duration)})
+        }
+    };
+
+    //Reset time and progress
+    const resetTimeAndProgress = () => {
+        dispatch({ type: 'player/UPDATE_CURRENT_TIME', payload: 0})
+        dispatch({ type: 'player/UPDATE_PROGRESS', payload: 0})
+    }
+
+    // Update Current Time and Progess
+    useEffect(() => {
+        if (player.isPlaying === true) {
+            timer = setInterval(() => {
+                const currenTime = Math.floor(myAudio.current.currentTime)
+                const progress = Math.floor(( myAudio.current.currentTime / player.duration) * 100)
+                dispatch({ type: 'player/UPDATE_CURRENT_TIME', payload: currenTime})
+                dispatch({ type: 'player/UPDATE_PROGRESS', payload: progress})
+            }, 1000);
+            return () => clearInterval(timer);
+        } 
+    }, [player.isPlaying])
+
+    
 
     // Update current track
     useEffect(() => {
         if (playlist.tracks.length > 0) {
             dispatch({ type: 'playlist/SET_CURRENT_TRACK', payload: player.trackIndex})
+            resetTimeAndProgress()
         }
     }, [player.trackIndex])
 
@@ -39,7 +69,7 @@ function Player(props) {
 
     return (
         <div className="c-player">
-            <audio ref={myAudio} src={playlist.currentTrack.src} >
+            <audio ref={myAudio} onLoadedMetadata={onLoadedMetadata} src={playlist.currentTrack.src} >
             </audio>
             <Controlls></Controlls>
         </div>
