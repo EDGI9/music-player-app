5478448d60aca1d1cc847fd3a9486a238a41bf60
Edgar Dias <dias.s.edgar@gmail.com>
2022-11-13 18:20:02 +0000

    Github actions test 2

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -9,46 +9,46 @@ function Player(props) {
     const playlist = useSelector(state => state.playlist)
     const global = useSelector(state => state.global)
     const dispatch = useDispatch()
-    const myAudio = useRef();
+    const myAudio = useRef()
   
     //Set duration of track
     const onLoadedMetadata = () => {
         if (myAudio.current) {
-            dispatch({ type: 'player/SET_DURATION', payload: Math.floor(myAudio.current.duration)});
+            dispatch({ type: 'player/SET_DURATION', payload: Math.floor(myAudio.current.duration)})
         }
-    };
+    }
 
     //Set duration of track
     const updateProgress = (currentTime, progress) => {
         if (player.isPlaying === true) {
-            dispatch({ type: 'player/UPDATE_CURRENT_TIME', payload: currentTime});
-            dispatch({ type: 'player/UPDATE_PROGRESS', payload: progress});
+            dispatch({ type: 'player/UPDATE_CURRENT_TIME', payload: currentTime})
+            dispatch({ type: 'player/UPDATE_PROGRESS', payload: progress})
         }
-    };
+    }
 
     //Start timer
     useEffect(() => {
         if (player.isPlaying === true) {
             const interval = setInterval(() => {
-                setTimer((prevTimer) => prevTimer + 1);
-            }, 1000);
+                setTimer((prevTimer) => prevTimer + 1)
+            }, 1000)
             
-            return () => clearInterval(interval);
+            return () => clearInterval(interval)
         }
     }, [player.isPlaying])
     
     // Update timer
     useEffect(() => {
         if (player.isPlaying === true) {
-            const currentTime = Math.floor(timer);
-            const progress = Math.floor(( timer / player.duration) * 100);
-            updateProgress(currentTime, progress);
+            const currentTime = Math.floor(timer)
+            const progress = Math.floor(( timer / player.duration) * 100)
+            updateProgress(currentTime, progress)
         }
     }, [timer])
 
     //Reset timer after track change
     useEffect(() => {
-        setTimer(0);
+        setTimer(0)
     }, [player.trackIndex])
 
     //Reset timer end of track
@@ -58,30 +58,30 @@ function Player(props) {
                 dispatch({ type: 'player/PAUSE'})
             } 
             //Reset everything
-            updateProgress(0, 0);
-            setTimer(0);
+            updateProgress(0, 0)
+            setTimer(0)
         }
     }, [player.currentTime])
 
 
     // Update artist info
     useEffect(async () => {
-        const artistInfo = await GET_ARTIST_INFO(global.token, playlist.currentTrack.artistId);
-        dispatch({ type: 'artist/SET_ARTIST_INFO', payload: artistInfo});
+        const artistInfo = await GET_ARTIST_INFO(global.token, playlist.currentTrack.artistId)
+        dispatch({ type: 'artist/SET_ARTIST_INFO', payload: artistInfo})
     }, [playlist.currentTrack.artistId])
 
     // Play/Pause the player
     useEffect(() => {
         if (player.isPlaying === true) {
-            myAudio.current.play();
+            myAudio.current.play()
         } else {
-            myAudio.current.pause();
+            myAudio.current.pause()
         }
     }, [player.isPlaying])
 
     // Mute/Unmute the player
     useEffect(() => {
-        myAudio.current.muted = player.isMuted;
+        myAudio.current.muted = player.isMuted
     }, [player.isMuted])
 
     return (
